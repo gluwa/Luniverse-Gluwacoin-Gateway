4907025d23730b6849fafbb75b0944a7f9a7e0b0
kevinnguyen17 <>
2021-10-23 04:42:45 -0400

    GLA-74 Update deployment script

--- migrations/2_deploy_gateway.js
+++ migrations/2_deploy_gateway.js
@@ -5,15 +5,35 @@ var USDCG_TOKEN_ADDRESS = "";
 var gluwaAddress = "";
 var luniverseAddress = "";
 
-module.exports = async function (deployer, network) {
-    // if (network == "rinkeby" || network == "rinkeby-fork")
-    // {
-    //     USDCG_TOKEN_ADDRESS = "0x71B7E714f87D8b46711a2533c9783d73386B8287";
-    //     gluwaAddress = "0x928ef75709ea7e8d112ed63b5c041dd9493f8448";
-    //     luniverseAddress = "0x697c720a3de7b4308f79d648b352c07b6d6a0b5b";
+var skip = false;
 
-    // }
-    // const instance = await deployProxy(LuniverseGluwacoinGateway, [USDCG_TOKEN_ADDRESS, gluwaAddress, luniverseAddress], { deployer, initializer: 'initialize' });
-    // console.log('Deployed ', instance.address);
-    // console.log('Token ' + (await instance.token()));       
-  };
+module.exports = async function (deployer, network) {
+    switch (network) {
+        case "rinkeby": {
+            USDCG_TOKEN_ADDRESS = "0x0aD1439A0e0bFdcD49939f9722866651a4AA9B3C";
+            gluwaAddress = "0xd9d097435E7CF8e663CcB26daB9C31A7F2B64ab4";
+            luniverseAddress = "0xd9d097435E7CF8e663CcB26daB9C31A7F2B64ab4";
+            break;
+        }
+
+        case "mainnet":
+            {
+                USDCG_TOKEN_ADDRESS = "0x1b9c206cf3df47c3b04941539b07f5784cbdde42";
+                gluwaAddress = "0x8cDFCaAD5df8A24f983882BaF461F33e1bC24000";
+                luniverseAddress = "0x8cDFCaAD5df8A24f983882BaF461F33e1bC24000";
+                break;
+            }
+
+        default:
+            {
+                skip = true;
+                break;
+            }
+
+    }
+    if (!skip) {
+        const instance = await deployProxy(LuniverseGluwacoinGateway, [USDCG_TOKEN_ADDRESS, gluwaAddress, luniverseAddress], { deployer, initializer: 'initialize' });
+        console.log('Deployed ', instance.address);
+        console.log('Token ' + (await instance.token()));
+    }
+};
